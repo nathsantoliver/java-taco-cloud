8adec9eea09040db71ce6564db7f6bc27672712c
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-02 12:48:16 -0300

    setting the validation

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -1,7 +1,6 @@
 package tacos.web;
 
-//import javax.validation.Valid;
-
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -46,21 +45,25 @@ public class DesignTacoController {
         }
     }
 
-    //tag::showDesignForm[]
     @GetMapping
     public String showDesignForm(Model model) {
         model.addAttribute("design", new Taco());
         return "design";
     }
 
-//    @PostMapping
-//    public String processDesign(Design design) {
-//        // Save the taco design...
-//        // We'll do this in chapter 3
-//        log.info("Processing dedsign: " + design);
-//
-//        return "redirect:/orders/current";
-//    }
+    @PostMapping
+    public String processDesign(@Valid @ModelAttribute("design") Taco design,
+                                Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+
+        // Save the taco design...
+        // We'll do this in chapter 3
+        log.info("Processing design: " + design);
+
+        return "redirect:/orders/current";
+    }
 
     private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
         return ingredients
